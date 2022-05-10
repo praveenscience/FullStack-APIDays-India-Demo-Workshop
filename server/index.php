6f79c9662c3ffcdc9a99bbb6231b07ba6f9e3d86
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2022-05-10 15:25:23 +0100

    Better programming Logic.

--- server/index.php
+++ server/index.php
@@ -1,8 +1,14 @@
 <?php
+  ob_start();
+  header("Content-type: application/json");
   $path = isset($_GET["path"]) && !empty($_GET["path"]) ? $_GET["path"] : "index";
-  if ($path === "index") {
-    echo "Home";
-  } else {
-    echo "Path: $path";
+  switch($path) {
+    case "index":
+      echo "Home";
+      break;
+    default:
+      http_response_code(404);
+      echo "Error 404";
+      break;
   }
 ?>
