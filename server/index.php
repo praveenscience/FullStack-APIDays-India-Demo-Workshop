dbb75eb5f9bde0762afebb583da3fb5f74e67c55
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2022-05-11 06:14:54 +0100

    Removed unnecessary path echo.

--- server/index.php
+++ server/index.php
@@ -3,7 +3,6 @@
   $data = json_decode(file_get_contents("./data.json"), true);
   header("Content-type: application/json");
   $path = isset($_GET["path"]) && !empty($_GET["path"]) ? $_GET["path"] : "index";
-  echo $path;
   switch(true) {
     case $path === "index":
       echo json_encode($data);
