f85b18dd2450f69e3ecd0e9ac7f6537f123f766c
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2022-05-11 06:13:43 +0100

    Checking if Path is a number.

--- server/index.php
+++ server/index.php
@@ -3,9 +3,13 @@
   $data = json_decode(file_get_contents("./data.json"), true);
   header("Content-type: application/json");
   $path = isset($_GET["path"]) && !empty($_GET["path"]) ? $_GET["path"] : "index";
+  echo $path;
   switch(true) {
     case $path === "index":
       echo json_encode($data);
+      break;
+    case is_numeric($path):
+      echo "Number Given";
       break;
     case $path === "time":
       echo json_encode(time() * 1000);
